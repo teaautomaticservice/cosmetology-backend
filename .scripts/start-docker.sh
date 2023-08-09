17c428758e3e64036796c44452b506e259dfeb73
DahakaLab <dahakalab@gmail.com>
2023-08-10 00:35:45 +0300

    Remove docker run in actions

--- .scripts/start-docker.sh
+++ .scripts/start-docker.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-docker stop nest-server
-docker rmi dahakalab/cosmetology-backend:latest
 docker image pull dahakalab/cosmetology-backend:latest
 docker run -d --rm --name nest-server -p 3000:3000 dahakalab/cosmetology-backend:latest
