a61472bb9db15d55ed3b85f50d89d4ab5a6f6b1a
DahakaLab <dahakalab@gmail.com>
2024-09-14 12:11:03 +0300

    Query int, parse boolean and number has been added

---
+++ src/ambient/parsers/parseBoolean.ts
@@ -0,0 +1,12 @@
+import { Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class ParseBoolean
+  implements PipeTransform<string | undefined, boolean | undefined>
+{
+  public transform(value?: string): boolean | undefined {
+    if (value) {
+      return value === 'true';
+    }
+  }
+}
---
+++ src/ambient/parsers/parseNumber.ts
@@ -0,0 +1,19 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class ParseNumber
+  implements PipeTransform<string | undefined, number | undefined>
+{
+  public transform(value?: string): number | undefined {
+    console.log('transform', value, typeof value);
+    if (typeof value === 'string') {
+      const result = Number(value);
+      if (Number.isNaN(result)) {
+        throw new BadRequestException(`Invalid value ${value}`);
+      }
+      return result;
+    }
+
+    return 10;
+  }
+}
