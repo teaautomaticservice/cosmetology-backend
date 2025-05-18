200ff7095a1327ff168e44c5d2336cd204ce8f77
DahakaLab <dahakalab@gmail.com>
2025-05-18 17:40:50 +0300

    User. Fix casse in user provider

--- src/domain/providers/users/users.provider.ts
+++ src/domain/providers/users/users.provider.ts
@@ -21,7 +21,7 @@ export class UsersProvider extends CommonPostgresqlProvider<UserEntity> {
     const matchedByEmail = await this.getByEmail(lowerEmail);
 
     if (matchedByEmail) {
-      throw new BadRequestException('Incorrect email.', { cause: { email: 'Please enter a valid email address.' } });
+      throw new BadRequestException('Incorrect email.', { cause: { email: ['Please enter a valid email address.'] } });
     }
 
     const resp = await this.create({
