a34f65a58384c907dc9ab8ab67c71ae739eb6172
DahakaLab <dahakalab@gmail.com>
2025-06-30 12:26:41 +0300

    mailer-setup Add common private method for sending email

--- src/domain/services/mailer/mailer.service.ts
+++ src/domain/services/mailer/mailer.service.ts
@@ -16,15 +16,34 @@ export class Mailer {
   }: {
     email: string;
   }): Promise<void> {
+    this.sendEmail({
+      email,
+      template: 'confirm-email',
+      subject: 'Confirm what you sweet bun',
+      context: {
+        title: 'Confirm what you very sweet bun! <3',
+        welcome: `Hello, ${email}!`
+      },
+    });
+  }
+
+  private async sendEmail({
+    email,
+    template,
+    subject,
+    context,
+  }: {
+    email: string;
+    template: string;
+    subject: string;
+    context: Record<string, string>;
+  }): Promise<void> {
     try {
       const result = await this.mailerService.sendMail({
         to: email,
-        template: 'confirm-email',
-        subject: 'Confirm what you sweet bun',
-        context: {
-          title: 'Confirm what you very sweet bun! <3',
-          welcome: `Hello, ${email}!`
-        }
+        template,
+        subject,
+        context,
       });
 
       this.logger.info('Mailer sendConfirmEmail', {
